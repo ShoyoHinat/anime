138bd85be0b2d19ef12f6172b9ca6c14eace10f2
ShoyoHinat <71874568+ShoyoHinat@users.noreply.github.com>
2020-09-28 14:40:53 +0800

    anime stuff

---
+++ main.js
@@ -0,0 +1,13 @@
+var hamburgerButton = document.querySelector('.hamburger_button');
+var mobileNav = document.querySelector('.mobile');
+
+function openMobile()  {
+    mobileNav.classList.add('open');
+}
+
+function openMobile()  {
+    mobileNav.classList.add('open');
+}
+
+hamburgerButton.addEventListener('click', openMobile);
+mobileNav.addEventListener('click', openMobile);
